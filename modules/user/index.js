8162387836d4fdd4a2d077e6c7f14b1754900e1f
hiru kalariya <kalariyahiren74@gmail.com>
2025-07-17 16:45:36 +0530

    refactor: comment out rate limiting middleware for OTP routes

--- modules/user/index.js
+++ modules/user/index.js
@@ -35,12 +35,22 @@ router
     update
   );
 router.get("/getMe", auth.authMiddleware, getMe);
-router.post("/sendOTP", otpSendLimit, sendOTPValidation, sendOTP);
-router.post("/resendOTP", resendOTPLimit, sendOTPValidation, resendOTP);
+router.post(
+  "/sendOTP",
+  //  otpSendLimit,
+  sendOTPValidation,
+  sendOTP
+);
+router.post(
+  "/resendOTP",
+  // resendOTPLimit,
+  sendOTPValidation,
+  resendOTP
+);
 
 router.post(
   "/verifyOTP",
-  otpVerificationLimit,
+  // otpVerificationLimit,
   verifyOTPValidation,
   auth.mobileProtected,
   verifyOTP
