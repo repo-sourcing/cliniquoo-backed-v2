521e9d99678e4dd0361a87a15877907892c0d75e
hiru kalariya <kalariyahiren74@gmail.com>
2025-08-13 15:29:48 +0530

    msg91 new reschul template add

--- utils/msg91.js
+++ utils/msg91.js
@@ -192,3 +192,23 @@ exports.sendWhatsAppAppointmentReminder = async (params) => {
     templateName: "appointment_reminder",
   });
 };
+
+/**
+ * Send WhatsApp appointment reschedule confirmation template via MSG91
+ * Uses static integrated number and namespace (overridable via env or params).
+ * @param {Object} params - Variables for the template
+ * @param {string[]} params.to - List of phone numbers as strings with country code (no +)
+ * @param {Array<string|number>} params.bodyValues - Values for body_1..body_5 in order
+ * @param {string} [params.languageCode="en"]
+ * @param {string} [params.policy="deterministic"]
+ * @param {string} [params.authKey]
+ * @param {string} [params.integratedNumber] - Optional override
+ * @param {string} [params.namespace] - Optional override
+ * @returns {Promise<any>} MSG91 API response data
+ */
+exports.sendWhatsAppAppointmentRescheduleConfirmation = async (params) => {
+  return sendWhatsAppTemplate({
+    ...params,
+    templateName: "appoitment_reschedual_confirmation",
+  });
+};
